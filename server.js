e0638c1c0193c6c8072ca7d86bb762dbff7ffd25
Mario Bross <magdalenamedina25@gmail.com>
2025-11-19 17:36:22 -0300

    primer commit

--- server.js
+++ server.js
@@ -1,23 +1,7 @@
-import express from "express";
-import morgan from "morgan";
-import dotenv from "dotenv";
+import app from "./src/app.js";
 
+const PORT = process.env.PORT || 3001;
 
-const app = express();
-
-//Middlewares
-dotenv.config({ quiet: true });
-app.use(morgan("dev"));
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-app.get("/api", (req, res) => {
-  res.send("welcome to the api");
-});
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000 🚀");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT} 🚀`);
 });
