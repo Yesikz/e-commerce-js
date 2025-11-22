c79a34c883311cfb6eba18ac543824dfab8f3546
Mario Bross <magdalenamedina25@gmail.com>
2025-11-22 18:57:00 -0300

    correción de errores de importación en controlador de productos

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -1,4 +1,4 @@
-import products from "../db/dataBases.js";
+import products from "../models/Productos.js";
 
 export const allProductsController = ()=>{
     if (!products || products.length === 0) {
