884958e978e838df9c804bcb15435b55df962fcc
Dev-Leandr0 <leandroalegregabriel@outlook.com>
2025-11-23 01:56:19 -0300

    feat(routes): agregar ruta /auth para register y login

--- src/routes/mainRoute.js
+++ src/routes/mainRoute.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import userRoutes from "./userRoutes.js";
 import productRoutes from "./productsRoutes.js";
 import categoryRoutes from "./categoryRoutes.js";
+import authRoutes from "./authRoutes.js";
 
 const mainRoute = Router();
 
@@ -13,5 +14,8 @@ mainRoute.use("/products", productRoutes);
 
 // Categoria
 mainRoute.use("/categories", categoryRoutes);
+
+// Auth (register / login)
+mainRoute.use("/auth", authRoutes);
 
 export default mainRoute;
